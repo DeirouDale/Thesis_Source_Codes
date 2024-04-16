8dcb2f60202b58f6172f89b0a673df656fb75e8d
TakkunPalacio <paltakkun176@gmail.com>
2024-04-17 01:17:54 +0800

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE gaitdata;
 CREATE TABLE IF NOT EXISTS assessment (
             id INTEGER PRIMARY KEY,
-            client_id INTEGER NOT NULL,
+            client_id VARCHAR(250) NOT NULL,
             name VARCHAR(255) NOT NULL,
             side VARCHAR(250) NOT NULL,
             assessment_num INTEGER,
@@ -13,3 +13,8 @@ CREATE TABLE IF NOT EXISTS assessment (
             ankle VARCHAR(250),
             insole VARCHAR(250)
         )
+
+INSERT INTO assessments (client_id, name, side, assessment_num, phase, image, frame, hips, knees, ankle, insole)
+VALUES
+  (2400001, 'Sha Boo', 'Left', 1, 2, 'image1.jpg', 1, 'normal', 'normal', 'normal', '101'),
+  (2400002, 'Hakk Dog', 'Right', 1, 2, 'image2.jpg', 1, 'normal', 'normal', 'normal', '111');

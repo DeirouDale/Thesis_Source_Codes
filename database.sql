d245f6f8c635ae4746d6cf4dfbaa400b66b08bd4
TakkunPalacio <paltakkun176@gmail.com>
2024-04-18 01:42:17 +0800

    Update database.sql

--- database.sql
+++ database.sql
@@ -5,8 +5,9 @@ CREATE TABLE IF NOT EXISTS patient_details(
 )
 CREATE TABLE IF NOT EXISTS assessment (
             id INTEGER PRIMARY KEY AUTO_INCREMENT,
-            client_id VARCHAR(250) NOT NULL FOREIGN KEY References patient_details(client_id),
+            client_id VARCHAR(250) NOT NULL,
             side VARCHAR(250) NOT NULL,
+            date_time DATE,
             assessment_num INTEGER,
             phase INTEGER,
             image VARCHAR(255),
@@ -14,13 +15,14 @@ CREATE TABLE IF NOT EXISTS assessment (
             hips VARCHAR(250),
             knees VARCHAR(250),
             ankle VARCHAR(250),
-            insole VARCHAR(250)
+            insole VARCHAR(250),
+            FOREIGN KEY (client_id) references patient_details(client_id)
         )
 
-INSERT INTO assessments (client_id, side, assessment_num, phase, image, frame, hips, knees, ankle, insole)
+INSERT INTO assessment (client_id, side, date_time, assessment_num, phase, image, frame, hips, knees, ankle, insole)
 VALUES
-  (2400001, 'Left', 1, 2, 'image1.jpg', 1, 'normal', 'normal', 'normal', '101'),
-  (2400002, 'Right', 1, 2, 'image2.jpg', 1, 'normal', 'normal', 'normal', '111');
+  (2400001, 'Left', '2024-04-17',1, 2, 'image1.jpg', 1, 'normal', 'normal', 'normal', '101'),
+  (2400002, 'Right','2024-04-17',1, 2, 'image2.jpg', 1, 'normal', 'normal', 'normal', '111');
 
 INSERT INTO patient_details(client_id,name) 
 VALUES
